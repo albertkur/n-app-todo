df4a2e14eb3393b27b19f5618ccfedb6f4c96887
Phanupong <103518876+inrongpon22@users.noreply.github.com>
2026-01-07 10:09:46 +0700

    update: filter users data
    - filter users data with search key
    - close modal when added user

--- src/client/pages/users/users-view-model.ts
+++ src/client/pages/users/users-view-model.ts
@@ -13,43 +13,66 @@ export class UsersViewModel extends PageViewModel
 {
     private readonly _users: Array<User> = new Array<User>();
     private _isModalDisplay: boolean = false;
+    private _searchKey: string = "";
 
     public get isModalDisplay(): boolean { return this._isModalDisplay; }
     public get getUsers(): Array<User> { return this._users; }
 
+    // filter users with search key
+    public get filterUsers(): Array<User>
+    {
+        let users = this._users;
+
+        if (this._searchKey.isNotEmptyOrWhiteSpace())
+        {
+            users = users.where((t) => `${t.firstName}${t.lastName}`.toLocaleLowerCase().contains(this._searchKey.toLowerCase()));
+        }
+        
+        return users;
+    }
+    
+    // handle search key
+    public get searchKey():string { return this._searchKey; }
+    public set searchKey(value: string) { this._searchKey = value; }
+    
     public constructor()
     {
         super();
+        // init set mock data
         this._users.push({
             id: Uuid.create(),
-            firstName: "Hello",
-            lastName: "User 1",
+            firstName: "John",
+            lastName: "Doe",
         },
             {
                 id: Uuid.create(),
-                firstName: "Hello",
-                lastName: "User 2",
+                firstName: "Jane",
+                lastName: "Doe",
             },
             {
                 id: Uuid.create(),
-                firstName: "Hello",
-                lastName: "User 3",
+                firstName: "Pea",
+                lastName: "Nut",
             });
 
     }
 
 
+    // add user
     public addUser(user: User): void
     {
         this._users.push(user);
+        this._isModalDisplay = false;
     }
-    
+
+    // open user modal form
     public openModal(): void
     {
         // display the user modal form
         this._isModalDisplay = true;
     }
-
+    
+    // close user modal form
     public closeModal(): void
     {
         // close the user modal form
