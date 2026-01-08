91678683000060a3dce84fc4f337e775a28c89b8
Phanupong <103518876+inrongpon22@users.noreply.github.com>
2026-01-08 10:50:46 +0700

    update: manage user
    - form validation
    - improve ui

--- src/client/pages/manage-user/manage-user-view-model.ts
+++ src/client/pages/manage-user/manage-user-view-model.ts
@@ -5,6 +5,7 @@ import { Routes } from "../routes";
 import { UserService } from "../../../sdk/services/user-service/user-service";
 import { inject } from "@nivinjoseph/n-ject";
 import { given } from "@nivinjoseph/n-defensive";
+import { Validator, strval } from "@nivinjoseph/n-validate";
 
 
 @template(require("./manage-user-view.html"))
@@ -18,9 +19,19 @@ export class ManageUserViewModel extends PageViewModel
 
     private _user: User | null = null;
     private _isNew: boolean;
+    private _firstName: string;
+    private _lastName: string;
+    private readonly _validator: Validator<this>;
 
     public get user(): User | null { return this._user; }
-    public get isNew(): boolean { return this._isNew; }
+    // handle user form
+    public get isNew(): boolean { return this._isNew; } // check is new create or update user information
+    public get firstName(): string { return this._firstName; }
+    public set firstName(value: string) { this._firstName = value; }
+    public get lastName(): string { return this._lastName; }
+    public set lastName(value: string) { this._lastName = value; }
+    public get hasErrors(): boolean { return !this._validate(); } // if has any error return true
+    public get errors(): object { return this._validator.errors; } // which field is error
 
     public constructor(userService: UserService, navigationService: NavigationService)
     {
@@ -32,23 +43,30 @@ export class ManageUserViewModel extends PageViewModel
         this._navigationService = navigationService;
 
         // init user data
-        this._user = {
-            firstName: "",
-            lastName: ""
-        };
+        // this._user = {
+        //     firstName: "",
+        //     lastName: ""
+        // };
         this._isNew = true;
+        this._firstName = "";
+        this._lastName = "";
+        this._validator = this._createValidator();
     }
 
     public async submit(): Promise<void>
     {
+        this._validator.enable();
+        if (!this._validate())
+            return;
+
         if (this.isNew)
         {
             console.log("create new user");
-            await this.addUser(this.user as User);
+            await this.addUser({ firstName: this._firstName, lastName: this._lastName } as User);
         }
         else
         {
-            await this.updateUser(this.user as User);
+            await this.updateUser({ ...this.user, firstName: this._firstName, lastName: this._lastName } as User);
         }
     }
 
@@ -105,14 +123,16 @@ export class ManageUserViewModel extends PageViewModel
                 firstName: "",
                 lastName: ""
             };
+            this._firstName = "";
+            this._lastName = "";
             return;
         }
 
         try
         {
-
-            // this._isNew = false;
             this._user = await this._userService.fetchUser(id);
+            this._firstName = this._user.firstName;
+            this._lastName = this._user.lastName;
             this._isNew = false;
         }
         catch (error)
@@ -120,5 +140,30 @@ export class ManageUserViewModel extends PageViewModel
             console.log("Something went wrong");
         }
 
+    }
+
+    private _validate(): boolean
+    {
+        this._validator.validate(this);
+        return this._validator.isValid;
+    }
+
+    private _createValidator(): Validator<this>
+    {
+        const validator = new Validator<this>(true);
+
+        validator
+            .prop("firstName")
+            .isRequired().withMessage("The first name field is required.")
+            .isString()
+            .useValidationRule(strval.hasMaxLength(50));
+
+        validator
+            .prop("lastName")
+            .isRequired().withMessage("The last name field is required.")
+            .isString()
+            .useValidationRule(strval.hasMaxLength(500));
+
+        return validator;
     }
 }
